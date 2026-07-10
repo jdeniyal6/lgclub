1c9a10abb993fec771fd356764be8a29672de440
jdeniyal6 <jdeniyal6@gmail.com>
2026-07-10 17:08:09 +0530

    Initial project setup

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <input type="email" placeholder="Email" />
+
+      <input type="password" placeholder="Password" />
+
+      <button>Login</button>
+    </div>
+  );
+}
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,15 @@
+export default function Signup() {
+  return (
+    <div>
+      <h1>Create Account</h1>
+
+      <input type="text" placeholder="Username" />
+
+      <input type="email" placeholder="Email" />
+
+      <input type="password" placeholder="Password" />
+
+      <button>Sign Up</button>
+    </div>
+  );
+}
